e5579671df4bcd7e91f42d75e164982934892718
evan <epmoring@gmail.com>
2022-10-11 20:14:52 -0700

    Cleaned up main loop

--- sequencer.py
+++ sequencer.py
@@ -30,6 +30,7 @@ intervals = {
     "M7":   15/8,
     "o":    2}
 
+
 def samples_to_seconds (samples):
     return samples / SAMPLE_RATE
 
@@ -88,6 +89,8 @@ def process_csv_row(row, bpm):
         arg_dict['samples'] = samples_from_beats(bpm, length)
     if 'freq' in row:
         arg_dict['freq'] = float(row['freq'])
+    if 'note' in row:
+        arg_dict['note'] = int(row['note'])
     waveform = signal_type(**arg_dict)
 
     return waveform, start
@@ -246,6 +249,10 @@ class Sine(Oscillator):
         sample_array = np.arange(0, end_angle , step_angle)
         wave_array = np.sin(sample_array)
         return wave_array 
+
+def Pentatonic(note, samples, gain = 1):
+    freq = major_pentatonic.scale[note]
+    return Sine(freq, samples, gain)
 
 class BentSine(Oscillator):
     def __init__(self, freq, freq2, samples, gain = 1):
@@ -429,6 +436,7 @@ signal_map = {
     "kick": Kick,
     "cymbal": Cymbal,
     "hihat": Hihat,
+    "pent": Pentatonic,
 }
 
 class Filter:
@@ -468,47 +476,8 @@ class LowPassFilter(Filter):
         yf[target_idx + 2: ] = 0
         return yf
 
-if __name__ == "__main__":
-    major_pentatonic = Scale(200,["1","M2","M3","5","M6","o"])
+def write_csv(file):
+    wf = csv_bpm(f"{file}.csv")
+    wf.write(f"{file}.wav")
 
-
-#    verse_a = csv_bpm("drums2.csv")
-#    verse_a.write("verse.wav")
-#    verse_b = csv_bpm("drums_verse_2.csv")
-#    verse_seq = Sequencer(16, seq_bpm)
-#    verse_seq.place_waveform(0, verse_a)
-#    verse_seq.place_waveform(8, verse_b)
-#    verse_fill = csv_bpm("verse_fill.csv")
-#    verse_fill_seq = Sequencer(16,seq_bpm)
-#    verse_fill_seq.place_waveform(0, verse_a)
-#    verse_fill_seq.place_waveform(8, verse_fill)
-#    full_verse = Sequencer(65,seq_bpm)
-#    full_verse.place_waveform(0, verse_seq)
-#    full_verse.place_waveform(16, verse_seq)
-#    full_verse.place_waveform(32, verse_seq)
-#    full_verse.place_waveform(48, verse_fill_seq)
-#    full_verse.write("verse.wav")
-#
-#    chorus_seq_a = Sequencer(16,seq_bpm)
-#    chorus_b = csv_bpm("chorus_b.csv")
-#    chorus_b.write("chorus_b.wav")
-#    chorus_seq_a.place_waveform(0, verse_a)
-#    chorus_seq_a.place_waveform(8, chorus_b)
-#    chorus_seq_a.write("chorus_b.wav")
-#
-#    chorus_seq_b = Sequencer(32,seq_bpm)
-#    chorus_fill = csv_bpm("chorus_fill.csv")
-#    chorus_seq_b.place_waveform(0, verse_a)
-#    chorus_seq_b.place_waveform(8, chorus_fill)
-#
-#    full_chorus = Sequencer(80, seq_bpm)
-#    full_chorus.place_waveform(0, chorus_seq_a)
-#    full_chorus.place_waveform(16, chorus_seq_a)
-#    full_chorus.place_waveform(32, chorus_seq_a)
-#    full_chorus.place_waveform(48, chorus_seq_b)
-#    full_chorus.write("full_chorus.wav")
-#
-#    loaded = load_csv("drums_verse_2.csv")
-#    #loaded.play()
-#    loaded.plot()
-#    loaded.write("drums.wav")
+major_pentatonic = Scale(200,["1","M2","M3","5","M6","o"])
