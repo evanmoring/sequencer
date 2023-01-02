495c67243c9d464afd7bff4ac34067cc4c179614
evan <epmoring@gmail.com>
2023-01-01 23:12:25 -0800

    Snare adding working

--- drum_machine_venv/hello.py
+++ drum_machine_venv/hello.py
@@ -6,26 +6,32 @@ sys.path.append('../')
 import sequencer
 import time
 
+bpm = 250
+beats = 16
+seq = sequencer.Sequencer(beats, bpm)
+#seq.place_waveform(4, sequencer.Snare())
+#seq.place_waveform(8, sequencer.Snare())
+
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World! {counter} {render_template('home.html')}</p>"
+    return f"<p>Hello, World! {render_template('home.html')}</p>"
 #    return f"<p>Hello, World! {counter}</p>"
 
 @app.route('/request', methods = ['POST'])
 def request_rec():
     print(request.json['type'])
+    print("X")
     print(request.json['x'])
+    x = request.json['x']
     #print(request.headers)
     data = 0
+
+    seq.place_waveform(x * beats, sequencer.Snare())
+    print("target beat")
+    print(x * beats)
     
-    if sine.freq > 600:
-        sine.freq = 300
-    else:
-        sine.freq = (sine.freq *1.5)
-    sine.array = sine.generate_waveform()
-    sine.apply_gain(1/ (sine.freq / 1000))
-    print(sine.freq)
+    
 
     return jsonify(isError= False,
                 message= "Success",
@@ -33,8 +39,7 @@ def request_rec():
                 data= data), 200
 
 def background_task():
-    sine.apply_gain(1/ (sine.freq / 1000))
-    sine.play()
+    seq.play()
 
 #    global counter
 #    while True:
@@ -43,8 +48,6 @@ def background_task():
         #print(counter)
 
 if __name__ == "__main__":
-    sine = sequencer.Sine(300,sequencer.seconds_to_samples(2))
-    counter = 5
     thread = threading.Thread(target=background_task)
     thread.daemon = True
     thread.start()
